cb41c95209bd2d224a309fd78bafeef020b8b20f
rzeraat.tur <rzeraat.tur@gmail.com>
2023-12-04 03:28:43 +0300

    add cors and clean the setup

--- TaskManagementAPI/Program.cs
+++ TaskManagementAPI/Program.cs
@@ -1,9 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add CORS services and define the policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAngularApp",
+                      policyBuilder =>
+                      {
+                          policyBuilder.WithOrigins("http://localhost:4200") // Replace with the actual URL of your Angular app
+                                       .AllowAnyHeader()
+                                       .AllowAnyMethod();
+                      });
+});
 
 var app = builder.Build();
 
@@ -16,29 +27,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// Use CORS with the policy defined above
+app.UseCors("AllowAngularApp");
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+// Map your API endpoints here
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+// OOP Concept: Record type for TaskItem, demonstrating encapsulation and immutability
+record TaskItem(int Id, string Title, string Description, bool IsComplete, DateOnly DueDate);
